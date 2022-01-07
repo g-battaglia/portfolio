7389df6df5bd68d6cc4846aeaf0776f8a1b8f503
g-battaglia <battaglia.giacomo@icloud.com>
2022-01-07 18:54:25 +0100

    Updates to i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 // the translations
 // (tip move them in a JSON file and import them,
@@ -20,6 +21,7 @@ const resources = {
 };
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
